7c366ac83db96639ac270d638776998acf69a994
Kelvin Jin <kelvinjin@google.com>
2017-12-07 14:44:03 -0800

    grpc-js: elaborate on grpc-js-core interface

--- packages/grpc-js/src/index.ts
+++ packages/grpc-js/src/index.ts
@@ -18,4 +18,42 @@
 
 'use strict';
 
-module.exports = require('@grpc/surface')(require('@grpc/js-core'));
+// const core = require('@grpc/js-core');
+
+import {
+  CallCredentials,
+  ChannelCredentials,
+  Metadata,
+  Status,
+  Client
+} from '@grpc/js-core';
+
+const compose = <S extends { compose: (other: T) => S }, T>(a: S, b: T) => a.compose(b);
+
+const notImplementedFn = () => { throw new Error('Not implemented'); }
+
+module.exports = require('@grpc/surface')({
+  Client,
+  Metadata,
+  status: Status,
+  credentials: {
+    createSsl: ChannelCredentials.createSsl,
+    createFromMetadataGenerator: CallCredentials.createFromMetadataGenerator,
+    createFromGoogleCredential: notImplementedFn,
+    combineChannelCredentials: (channelCredentials: ChannelCredentials,
+        ...callCredentialsList: CallCredentials[]): ChannelCredentials => {
+      return callCredentialsList.reduce(compose, channelCredentials);
+    },
+    combineCallCredentials: (...callCredentialsList: CallCredentials[]): CallCredentials => {
+      return callCredentialsList.slice(1).reduce(compose, callCredentialsList[0]);
+    },
+    createInsecure: ChannelCredentials.createInsecure
+  },
+  get setLogger() { return notImplementedFn; },
+  get setLogVerbosity() { return notImplementedFn; },
+  get ServerCredentials() { return notImplementedFn; },
+  get propagate() { return notImplementedFn; },
+  get callError() { return notImplementedFn; },
+  get writeFlags() { return notImplementedFn; },
+  get logVerbosity() { return notImplementedFn; },
+});
